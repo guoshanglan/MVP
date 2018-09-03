bcce3a7c947e759b6408e979c3c0c6b0273f586a
yaorui <yaorui@huodongxing.com>
2018-09-03 09:55:59 +0800

    修改登陆界面

--- app/src/main/java/com/example/administrator/hdx_conference_mvp/fragment/HomeFragment.java
+++ app/src/main/java/com/example/administrator/hdx_conference_mvp/fragment/HomeFragment.java
@@ -1,6 +1,8 @@
 package com.example.administrator.hdx_conference_mvp.fragment;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
@@ -9,6 +11,7 @@ import android.view.ViewGroup;
 
 import com.example.administrator.hdx_conference_mvp.R;
 import com.example.administrator.hdx_conference_mvp.adapter.HomeAdapter;
+import com.example.administrator.hdx_conference_mvp.adapter.HomeBannerAdapter;
 import com.example.administrator.hdx_conference_mvp.base.BaseFragment;
 import com.example.administrator.hdx_conference_mvp.base.BasePresenter;
 import com.example.administrator.hdx_conference_mvp.widview.CirclePageIndicator;
@@ -31,6 +34,10 @@ public class HomeFragment extends BaseFragment implements XListView.IXListViewLi
     public CirclePageIndicator pageIndicator;  //圆点控件
     public HomeAdapter adapter;
     public List<String>list;
+    public List<String>bannerlist;
+
+    private static final int SCROLL_WHAT = 0;
+    private static final int interval = 5000;//banner 滑动间隔时间
 
 
 
@@ -51,10 +58,19 @@ public class HomeFragment extends BaseFragment implements XListView.IXListViewLi
         pageIndicator = (CirclePageIndicator) headview.findViewById(R.id.cicleIndicator);
         mViewPager = (ViewPager) headview.findViewById(R.id.viewpager);
         list=new ArrayList<>();
+        bannerlist=new ArrayList<>();
+        bannerlist.add("1");
+        bannerlist.add("1");
+        bannerlist.add("1");
+        bannerlist.add("1");
         list.add("ahui");
         list.add("ahui");
         list.add("ahui");
         list.add("ahui");
+        mViewPager.setAdapter(new HomeBannerAdapter(getActivity(), bannerlist));
+        pageIndicator.setViewPager(mViewPager);
+        sendScrollMessage(interval);//banner开始滚动
+
         adapter=new HomeAdapter(getActivity(),list);
         mListView.setAdapter(adapter);
         mListView.addHeaderView(headview);
@@ -71,6 +87,29 @@ public class HomeFragment extends BaseFragment implements XListView.IXListViewLi
         super.onDestroy();
 
     }
+
+
+
+    private void sendScrollMessage(long delayTimeInMills) {
+        handler.removeMessages(SCROLL_WHAT);
+        handler.sendEmptyMessageDelayed(SCROLL_WHAT, delayTimeInMills);
+    }
+
+
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            int count = bannerlist.size();
+            int currentIndex = mViewPager.getCurrentItem();
+            if (count > 1) {
+                currentIndex = (currentIndex + 1) % count;
+                mViewPager.setCurrentItem(currentIndex);
+                sendScrollMessage(interval);
+            }
+        }
+
+        ;
+    };
 
     @Override
     public void onRefresh() {
